08b9ec4756a26bbc71e8c25801564998cc61c7b9
Paulina Rzemieniarz <p.rzemieniarz@gmail.com>
2025-12-05 15:36:58 +0100

    publish subscribe (exchange)

---
+++ PublishSubscribe/EmitLog/EmitLog.cs
@@ -0,0 +1,27 @@
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+    exchange: "logs",
+    type: ExchangeType.Fanout); //fanout wysyla wszystko co dostaje do wszystkich queues ktore zna
+
+var message = GetMessage(args);
+var body = Encoding.UTF8.GetBytes(message);
+await channel.BasicPublishAsync(
+    exchange: "logs",
+    routingKey: string.Empty,
+    body: body);
+
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
+static string GetMessage(string[] args)
+{
+    return ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World!");
+}
